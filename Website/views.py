e03ad75f8f2c2945a66b8a127f9ef87c4b6d14f0
Marios Philippou <mariosphilippou145@gmail.com>
2024-09-16 13:08:18 +0300

    Create & Delete Notes

--- Website/views.py
+++ Website/views.py
@@ -1,11 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 #make this file to be a blueprint file (have routes in this)
 views = Blueprint('views', __name__)
 
 #home endpoint
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('New note created', category='success')
     return render_template("home.html", user=current_user)
+
+#handle delete note
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
