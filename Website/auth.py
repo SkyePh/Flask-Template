296249afbba3b6c6331dea6e778eb9f2c614b93b
SkyePh <mariosphilippou145@gmail.com>
2024-09-16 10:18:14 +0300

    Make routes Make Python Package Make Sign Up Page

---
+++ Website/auth.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, render_template, request, flash
+
+#make this file to be a blueprint file (have routes in this)
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+
+    if request.method == "POST":
+        email = request.form.get('email')
+        first_name = request.form.get('first_name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash("Email must be greater than 3 characters", category="error")
+        elif len(first_name) < 2:
+            flash("First name must be greater than 1 characters", category="error")
+        elif password1 != password2:
+            flash("Passwords must match", category="error")
+        elif len(password1) < 7:
+            flash("Password must be at least 7 characters", category="error")
+        else:
+            flash("You have successfully registered", category="success")
+            #add account to db
+            pass
+
+    return render_template("sign_up.html")
