ffbc425c5735385c62aa63429e153308aeca7300
SkyePh <mariosphilippou145@gmail.com>
2024-09-16 10:53:34 +0300

    Setup DB TODO: Link SignUp

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+#create db
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'fueh carti'
+    #store db in website file
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + DB_NAME
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -10,4 +19,17 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    create_db(app)
+
     return app
+
+
+##check if db is already created. if not then create it
+def create_db(app):
+    with app.app_context():
+
+        if not path.exists('Website/' + DB_NAME):
+            db.create_all()
+            print('Database created')
